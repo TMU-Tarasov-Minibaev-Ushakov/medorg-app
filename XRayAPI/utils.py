63ed7fe2773768aee4a518903fbc51f2fa0f358e
Ushakov Egor <usakovegor2002@gmail.com>
2024-03-28 13:43:55 +0300

    fix: color converting problems

--- XRayAPI/utils.py
+++ XRayAPI/utils.py
@@ -16,7 +16,10 @@ loader = transforms.Compose([transforms.Resize(255),
 
 def imageLoader(image):
     img = Image.open(io.BytesIO(image)).convert("RGB")
-    img = loader(img).float()
+    np_image = np.array(img)
+    bgr_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
+    pil_image = Image.fromarray(bgr_image)
+    img = loader(pil_image).float()
     img = img.unsqueeze(0)
     return img
 
